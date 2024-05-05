a51cba067d4ff4a07126a7f453a89c2a6f1307ed
Rafaela Ilarri <r_ilarri@mail.fhsu.edu>
2024-05-05 17:47:20 -0400

    Version 6 RI

--- routes/api/states.js
+++ routes/api/states.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const statesController = require('../../controllers/statesController');
 
-router.get('/', statesController.getAllStates);
+// Define routes for state-specific endpoints
 router.get('/:state', statesController.getStateByCode);
 router.get('/:state/funfact', statesController.getRandomFunFact);
 router.get('/:state/capital', statesController.getStateCapital);
